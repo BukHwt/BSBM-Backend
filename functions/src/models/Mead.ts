cdd262e791c86225b1d7fa1fda7bc748a73d3aef
Steve Anderson <swanderson79@gmail.com>
2022-08-03 15:47:30 -0400

    minor adjustment to Mead model

--- functions/src/models/Mead.ts
+++ functions/src/models/Mead.ts
@@ -2,6 +2,17 @@ import { ObjectId } from "mongodb";
 
 export default interface Mead {
   _id?: ObjectId;
+  brewName: string;
+  style: string;
   author: string;
-  name: string;
+  authorUID: string;
+  volume: number;
+  brewDate: string;
+  honey: string;
+  honeyVariety: string;
+  honeyAmount: number;
+  base: string;
+  baseAmount: number;
+  OG: number;
+  FG: number;
 }
